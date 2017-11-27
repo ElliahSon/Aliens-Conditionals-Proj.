bc47e22ffe4951997a4a84fb97ced4edab3845b8
ElliahSon <30949066+ElliahSon@users.noreply.github.com>
2017-11-27 10:45:54 -1000

    done

--- ree.js
+++ ree.js
@@ -1,26 +1,25 @@
 //Green Alien
-var x = 5000;
+var x = 6500;
 
 //Red Alien
-var y = 3000;
+var y = 1501;
 
 //Blue Alien
-var z = 2000;
+var z = 1999;
 var warning;
 function meanMachine (green, red, blue) {
   if ((x + y + z) == 10000) {
-      warning = "There is 10,000 aliens.";
+      console.log("There is 10,000 aliens.");
       
   } if (x > 5000) {
-      warning = "MOTHER OF GOD, not the green ones!";
+      console.log("MOTHER OF GOD, not the green ones!");
       
   } if ((x + y) > 8000) {    
-      warning = "DOOOOOM!";
+      console.log("DOOOOOM!");
       
   } if ((y + z) < 3000) {
-    warning = "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!";
+      console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
       
   }
 } 
 meanMachine(x, y, z);
-console.log(warning);
