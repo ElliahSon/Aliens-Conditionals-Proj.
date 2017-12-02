3c9535915cbea9edcdb80229c4e6529a0ca9bf35
ElliahSon <30949066+ElliahSon@users.noreply.github.com>
2017-12-01 14:17:29 -1000

    Add files via upload

--- ree.js
+++ ree.js
@@ -9,17 +9,20 @@ var z = 1999;
 
 function meanMachine (green, red, blue) {
   if ((x + y + z) == 10000) {
-      console.log("There is 10,000 aliens.");
+      console.log("There is 10,000 aliens. おまえ は もう しんでいる。");
+
+      if (x > 5000) {
+        console.log("MOTHER OF GOD, not the green ones!");
       
-  } if (x > 5000) {
-      console.log("MOTHER OF GOD, not the green ones!");
+      } if ((x + y) > 8000) {    
+        console.log("DOOOOOM!");
       
-  } if ((x + y) > 8000) {    
-      console.log("DOOOOOM!");
-      
-  } if ((y + z) < 3000) {
-      console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
-      
+      } if ((y + z) < 3000) {
+        console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
+      }
+
+  } if ((x + y + z) < 10000) {
+    console.log("There aint even 10000 aliens.")
   }
 } 
 meanMachine(x, y, z);
